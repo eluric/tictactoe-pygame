6a212d6c2e05eb4c89ae017236987d23da91977f
Adam Zhang <azha3852@uni.sydney.edu.au>
2022-06-17 22:01:04 +1000

    Implemented check_win function that checks if either player has won. Currently, the game closes as soon as a player wins, before even drawing the final cross/circle that was placed.

--- tictactoe.py
+++ tictactoe.py
@@ -20,6 +20,29 @@ def h_line_coords(
 
     return h_line_x, h_line_y
 
+# checks if the player is clicking in a grid and which grid they're clicking on
+def check_click(
+    mouse_pos: tuple, 
+    num_rows: int, num_columns: int,
+    x_boundaries: list[str], y_boundaries: list[str],
+    state: list[list[str]], x_turn: bool
+) -> bool:
+    mouse_x, mouse_y = mouse_pos
+
+    for i in range(num_rows):
+        if mouse_y >= y_boundaries[i] and mouse_y < y_boundaries[i+1]:
+            for j in range(num_columns):
+                if mouse_x >= x_boundaries[j] and mouse_x < x_boundaries[j+1]:
+                    if state[i][j] != "":
+                        return
+                    
+                    if x_turn:
+                        state[i][j] = "X"
+                        return False, (i, j)
+                    else:
+                        state[i][j] = "O"
+                        return True, (i, j)
+
 def xo_coords(
     row: int, column: int,
     top_left_x: int, top_left_y: int,
@@ -29,6 +52,105 @@ def xo_coords(
     y = top_left_y + grid_line_length * row/3
 
     return x, y
+
+# checks if either player has won
+# returns a tuple of two booleans
+# (x_wins, o_wins)
+# if return value is (True, False), then x has won
+def check_win(state: list[list[str]], last_move: tuple[int, int], num_rows: int, num_columns: int) -> tuple:
+    # list of all the conditions where a player can win
+    # win_conditions = [
+    #     # horizontal wins
+    #     [(0, 0), (0, 1), (0, 2)],
+    #     [(1, 0), (1, 1), (1, 2)],
+    #     [(2, 0), (2, 1), (2, 2)],
+
+    #     # vertical wins
+    #     [(0, 0), (1, 0)]
+    # ]
+
+    # only need to check the horizontal, vertical and diagonal of the last move
+    # if no player has won until that point, then none of the previous moves were winning moves
+    # therefore, if a player has won this round, then only the last move placed will result in a win
+    if not last_move:
+        return False, False
+
+    row, column = last_move
+    
+    horizontal_win = True
+    # first, check the row
+    for i in range(num_columns - 1):
+        if state[row][i] != state[row][i+1]:
+            horizontal_win = False
+            break
+    
+    if horizontal_win:
+        if state[row][0] == "O":
+            return False, True
+        else:
+            return True, False
+    
+    
+    vertical_win = True
+    # check column
+    for i in range(num_rows - 1):
+        if state[i][column] != state[i+1][column]:
+            vertical_win = False
+            break
+
+    if vertical_win:
+        if state[0][column] == "O":
+            return False, True
+        else:
+            return True, False
+
+    """
+    If the row and column are the same, then we know that the last move placed was
+    in range of the down diagonal and we need to check the down diagonal.
+    e.g (0, 0), (1, 1), (2, 2)
+    O| | 
+    ------
+     |O| 
+    ------
+     | |O
+
+    If the row and column add up to to equal the total number of rows or total number of columns,
+    then we know that the last move was in range of the up diagonal.
+    e.g (3, 0), (2, 1), (0, 3). The total number of rows is 3.
+     | |O
+    ------
+     |O| 
+    ------
+    O| |
+    """
+    down_diagonal_win = True
+    if row == column:
+        for i in range(num_rows - 1):
+            if state[i][i] != state[i+1][i+1]:
+                down_diagonal_win = False
+                break
+
+        if down_diagonal_win:
+            if state[0][0] == "O":
+                return False, True
+            else:
+                return True, False
+
+    
+    up_diagonal_win = True
+    if row + column == num_rows:
+        for i in range(num_rows - 1):
+            if state[i][num_rows-i] != state[i+1][num_rows-i-1]:
+                up_diagonal_win = False
+                break
+    
+        if up_diagonal_win:
+            if state[num_rows-1][0] == "O":
+                return False, True
+            else:
+                return True, False
+
+    return False, False
 
 def main():
     pygame.init()
@@ -63,11 +185,6 @@ def main():
     h_line_x1, h_line_y1 = h_line_coords(1, screen_width, screen_height, grid_line_length)
     h_line_x2, h_line_y2 = h_line_coords(2, screen_width, screen_height, grid_line_length)
 
-    # describes the coordinates at the top left of each of the grid squares
-    # grid_pos = [
-    #     [(j, i) for i in range(4) for j in range(k, k+1)] for k in range(4)
-    # ]
-
     y_boundaries = [
         v_line_y1 + grid_line_length * i/3 for i in range(4)
     ]
@@ -82,7 +199,9 @@ def main():
     ]
 
     x_turn = True
-
+    x_wins = False
+    o_wins = False
+    last_move = ()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -90,29 +209,13 @@ def main():
                 return
 
             if event.type == pygame.MOUSEBUTTONUP:
-                mouse_x, mouse_y = pygame.mouse.get_pos()
-                done = False
-                for i in range(num_rows):
-                    if mouse_y >= y_boundaries[i] and mouse_y < y_boundaries[i+1]:
-                        for j in range(num_columns):
-                            if mouse_x >= x_boundaries[j] and mouse_x < x_boundaries[j+1]:
-                                if state[i][j] != "":
-                                    done = True
-                                    break
-                                
-                                if x_turn:
-                                    state[i][j] = "X"
-                                    x_turn = False
-                                else:
-                                    state[i][j] = "O"
-                                    x_turn = True
-
-                                done = True
-
-                                print(state)
-                        
-                        if done:
-                            break
+                # mouse_x, mouse_y = pygame.mouse.get_pos()
+                x_turn, last_move = check_click(
+                    pygame.mouse.get_pos(), 
+                    num_rows, num_columns, 
+                    x_boundaries, y_boundaries, 
+                    state, x_turn
+                )
         
         # draw the grid lines for tic tac toe
         screen.blit(v_line, (v_line_x1, v_line_y1))
@@ -131,6 +234,15 @@ def main():
                 
                 else:
                     screen.blit(circle_img, xo_coords(i, j, h_line_x1, v_line_y1, grid_line_length))
+
+        x_wins, o_wins = check_win(state, last_move, num_rows, num_columns)
+
+        if x_wins:
+            print("X wins!")
+            return
+        elif o_wins:
+            print("O wins!")
+            return
 
         pygame.display.update()
         clock.tick(60)
