81ef62e9855d41322fcd2a8c843740aaf54496f7
Adam Zhang <azha3852@uni.sydney.edu.au>
2022-06-17 18:57:21 +1000

    Game now places a cross/circle on a grid space depending on whose turn it is and where they click on the grid. The circle is slightly off-centre.

--- tictactoe.py
+++ tictactoe.py
@@ -20,6 +20,16 @@ def h_line_coords(
 
     return h_line_x, h_line_y
 
+def xo_coords(
+    row: int, column: int,
+    top_left_x: int, top_left_y: int,
+    grid_line_length: int
+) -> tuple:
+    x = top_left_x + grid_line_length * column/3
+    y = top_left_y + grid_line_length * row/3
+
+    return x, y
+
 def main():
     pygame.init()
     screen_width = 1200
@@ -28,10 +38,19 @@ def main():
     pygame.display.set_caption("Tic Tac Toe")
     clock = pygame.time.Clock()
 
+    circle_img = pygame.image.load("circle.png")
+    cross_img = pygame.image.load("cross.png")
+
+    # scale images
+    circle_img = pygame.transform.scale(circle_img, (240, 240))
+    cross_img = pygame.transform.scale(cross_img, (240, 240))
+
     grid_line_length = 720
     grid_line_width = 5
 
-    
+    num_rows = 3
+    num_columns = 3
+
     v_line = pygame.Surface((grid_line_width, grid_line_length))
     v_line.fill("White")
 
@@ -45,36 +64,73 @@ def main():
     h_line_x2, h_line_y2 = h_line_coords(2, screen_width, screen_height, grid_line_length)
 
     # describes the coordinates at the top left of each of the grid squares
-    grid_pos = [
-        [(i, j) for i in range(4) for j in range(k, k+1)] for k in range(4)
-    ]
+    # grid_pos = [
+    #     [(j, i) for i in range(4) for j in range(k, k+1)] for k in range(4)
+    # ]
 
     y_boundaries = [
         v_line_y1 + grid_line_length * i/3 for i in range(4)
     ]
-
     x_boundaries = [
         h_line_x1 + grid_line_length * i/3 for i in range(4)
     ]
+
+    state = [
+        ["", "", ""],
+        ["", "", ""],
+        ["", "", ""]
+    ]
+
+    x_turn = True
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+
+            if event.type == pygame.MOUSEBUTTONUP:
+                mouse_x, mouse_y = pygame.mouse.get_pos()
+                done = False
+                for i in range(num_rows):
+                    if mouse_y >= y_boundaries[i] and mouse_y < y_boundaries[i+1]:
+                        for j in range(num_columns):
+                            if mouse_x >= x_boundaries[j] and mouse_x < x_boundaries[j+1]:
+                                if state[i][j] != "":
+                                    done = True
+                                    break
+                                
+                                if x_turn:
+                                    state[i][j] = "X"
+                                    x_turn = False
+                                else:
+                                    state[i][j] = "O"
+                                    x_turn = True
+
+                                done = True
+
+                                print(state)
+                        
+                        if done:
+                            break
         
+        # draw the grid lines for tic tac toe
         screen.blit(v_line, (v_line_x1, v_line_y1))
         screen.blit(v_line, (v_line_x2, v_line_y2))
         screen.blit(h_line, (h_line_x1, h_line_y1))
         screen.blit(h_line, (h_line_x2, h_line_y2))
 
-        mouse_x, mouse_y = pygame.mouse.get_pos()
+        # draw the circles and crosses
+        for i in range(num_rows):
+            for j in range(num_columns):
+                if state[i][j] == "":
+                    continue
 
-        for i in range(3):
-            if mouse_y >= y_boundaries[i] and mouse_y < y_boundaries[i+1]:
-                for j in range(3):
-                    if mouse_x >= x_boundaries[j] and mouse_x < x_boundaries[j+1]:
-                        print(grid_pos[i][j])
+                if state[i][j] == "X":
+                    screen.blit(cross_img, xo_coords(i, j, h_line_x1, v_line_y1, grid_line_length))
+                
+                else:
+                    screen.blit(circle_img, xo_coords(i, j, h_line_x1, v_line_y1, grid_line_length))
 
         pygame.display.update()
         clock.tick(60)
